690b6f081c9b80cb8e31268c848c6c9660691f48
FilipMajvelder <filip.majvelder@seznam.cz>
2026-04-09 10:43:53 +0200

    Ukol az po vytvoreni endpointu pro Bucket

---
+++ models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import String, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, MappedColumn
+from datetime import datetime
+
+from database import Base
+
+
+class FileRecord(Base):
+    __tablename__ = "files"
+    
+    id: Mapped[str] = MappedColumn(String, primary_key=True)
+    user_id: Mapped[str] = MappedColumn(String, index=True)
+    filename: Mapped[str] = MappedColumn(String)
+    path: Mapped[str] = MappedColumn(String)
+    size: Mapped[int] = MappedColumn()
+    created_at: Mapped[datetime] = MappedColumn(DateTime, default=datetime.utcnow)
+
+    bucket_id: Mapped[str] = MappedColumn(String, ForeignKey("buckets.id"))
+
+
+class Bucket(Base):
+    __tablename__ = "buckets"
+
+    id: Mapped[str] = MappedColumn(String, primary_key=True)
+    name: Mapped[str] = MappedColumn(String, unique=True)
+    created_at: Mapped[datetime] = MappedColumn(DateTime, default=datetime.utcnow)
